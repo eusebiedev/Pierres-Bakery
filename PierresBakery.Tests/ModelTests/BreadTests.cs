64a71bdfe0d9b155619a25d15f5944bf9698a1f8
Eusebie Siebenberg <siebenee@gmail.com>
2023-05-05 10:30:20 -0700

    WIP, does not return cost per loaf

--- PierresBakery.Tests/ModelTests/BreadTests.cs
+++ PierresBakery.Tests/ModelTests/BreadTests.cs
@@ -12,9 +12,13 @@ namespace PierresBakery.Tests
       Bread newBread = new Bread();
       Assert.AreEqual(typeof(Bread), newBread.GetType());
     }
-
-
-
+    [TestMethod]
+    public void BreadOrder_TotalForOneBread_Bread()
+    {
+      Bread newBread = new Bread();
+      int loafOrder = 1;
+      Assert.AreEqual(5, Bread.TotalBreadCost(loafOrder));
+    }
 
   }
 }
