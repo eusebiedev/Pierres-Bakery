54bcf7cecbe881e43f6bab0b3976daf6517d4d10
Eusebie Siebenberg <siebenee@gmail.com>
2023-05-05 15:20:03 -0700

    Add logic for 1 pasty cost to Pastry.cs, Pass Test

--- PierresBakery/Models/Pastry.cs
+++ PierresBakery/Models/Pastry.cs
@@ -4,6 +4,17 @@ namespace PierresBakery.Models
 {
   public class Pastry
   {
-    
+    public int PastryCost { get; set; }
+    public static int PastryOrder(int pastryOrder)
+    { 
+      int PastryCost = 2;
+      int PastrySubTotal = 0;
+      // int Discount = 2;
+      if (pastryOrder == 1)
+      {
+        PastrySubTotal = (pastryOrder * PastryCost);
+      }
+      return PastrySubTotal;
+    }
   }
 } 
