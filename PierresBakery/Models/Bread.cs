c6fcab911ca1e9b1b5f60aad20276c77a43aa1a6
Eusebie Siebenberg <siebenee@gmail.com>
2023-05-05 12:03:08 -0700

    Add logic for breadorder, total, beginning if statment to bread.cs file

--- PierresBakery/Models/Bread.cs
+++ PierresBakery/Models/Bread.cs
@@ -4,12 +4,16 @@ namespace PierresBakery.Models
 {
   public class Bread
   {
-    public int LoafCost { get; set; } 
-    // public static int TotalBreadCost(int loafOrder)
-    
-    // int LoafCost = 5;
-
-    // return TotalBreadCost;
-    
+    // public int LoafCost { get; set; }
+    public static int BreadOrder(int loafOrder)
+    {
+      int BreadOrder = 1;
+      int Total = 5;
+      if (BreadOrder == 3)
+      {
+        
+      }
+      return Total;
+    }
   }
 }
