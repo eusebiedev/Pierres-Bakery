ce5258ad9ed71bef724f324446f0c627771e51f2
Eusebie Siebenberg <siebenee@gmail.com>
2023-05-05 16:59:05 -0700

    Add UI to Program.cs

--- PierresBakery/Program.cs
+++ PierresBakery/Program.cs
@@ -1,6 +1,6 @@
 using System;
-using PierresBakery.Models;
 using System.Collections.Generic;
+using PierresBakery.Models;
 
 namespace PierresBakery 
 {
@@ -8,7 +8,18 @@ namespace PierresBakery
   {
     static void Main()
     {
-
+      Console.WriteLine("========*========*========**========**========*");
+      Console.WriteLine("Welcome to Pierre's Bread & Pastry Bakery");
+      Console.WriteLine("========**========**========**========**======*");
+      Console.WriteLine("Our Bread is $5 a loaf, buy 2 and get one FREE!");
+      Console.WriteLine("Our fine Pastries are $2 each, buy 3 and get one FREE!");
+      Console.WriteLine("========**========**========**========**======*");
+      Console.WriteLine("How many loaves of Bread can we get you?");
+      int Bread = int.Parse(Console.ReadLine());
+      Console.WriteLine("How many fine Pastries can we get you?");
+      int Pastry = int.Parse(Console.ReadLine());
+      int orderTotal = Bread + Pastry;
+      Console.WriteLine("Thanks so much, your Order for {Bread} Bread & {Pastry} Pastries comes to: $ ${orderTotal} ");
     }
   }
 
