9152f2e46ccc9b30b24b2191000261a6c3275102
Eusebie Siebenberg <siebenee@gmail.com>
2023-05-05 14:34:57 -0700

    Add test for 7 loaves with discount

--- PierresBakery.Tests/ModelTests/BreadTests.cs
+++ PierresBakery.Tests/ModelTests/BreadTests.cs
@@ -12,36 +12,47 @@ namespace PierresBakery.Tests
       Bread newBread = new Bread();
       Assert.AreEqual(typeof(Bread), newBread.GetType());
     }
+
     [TestMethod]
     public void BreadOrder_TotalForOneBread_LoafCost()
     {
       Assert.AreEqual(5, Bread.BreadOrder(1));
     }
+
     [TestMethod]
     public void BreadOrder_TotalForTwoBread_LoafCost()
     {
       Assert.AreEqual(10, Bread.BreadOrder(2));
     }
+
     [TestMethod]
     public void BreadOrder_TotalForThreeBread_LoafCost()
     {
       Assert.AreEqual(10, Bread.BreadOrder(3));
     }
+
     [TestMethod]
     public void BreadOrder_TotalForFourBread_LoafCost()
     {
       Assert.AreEqual(15, Bread.BreadOrder(4));
     }
+
     [TestMethod]
     public void BreadOrder_TotalForFiveBread_LoafCost()
     {
       Assert.AreEqual(20, Bread.BreadOrder(5));
     }
+
     [TestMethod]
     public void BreadOrder_TotalForSixBread_LoafCost()
     {
       Assert.AreEqual(20, Bread.BreadOrder(6));
     }
 
+    [TestMethod]
+    public void BreadOrder_TotalForSevenBread_LoafCost()
+    {
+      Assert.AreEqual(25, Bread.BreadOrder(7));
+    }
   }
 }
