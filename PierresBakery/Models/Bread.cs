94d9d190c5308bf458ae9cdb5f79d0fad39e435e
Eusebie Siebenberg <siebenee@gmail.com>
2023-05-05 10:43:53 -0700

    This commmit will show get/set for LoafCoast on Bread.cs

--- PierresBakery/Models/Bread.cs
+++ PierresBakery/Models/Bread.cs
@@ -4,6 +4,12 @@ namespace PierresBakery.Models
 {
   public class Bread
   {
+    public int LoafCost { get; set; } 
+    // public static int TotalBreadCost(int loafOrder)
+    
+    // int LoafCost = 5;
+
+    // return TotalBreadCost;
     
   }
-} 
+}
