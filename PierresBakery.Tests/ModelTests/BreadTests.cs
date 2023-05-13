fe3c2ca79273bbe41b811014fa273915bbd8bea8
Eusebie Siebenberg <siebenee@gmail.com>
2023-05-13 12:14:24 -0700

    Update tests to account for larger orders

--- PierresBakery.Tests/ModelTests/BreadTests.cs
+++ PierresBakery.Tests/ModelTests/BreadTests.cs
@@ -9,72 +9,82 @@ namespace PierresBakery.Tests
     [TestMethod]
     public void BreadConstructor_CreatesInstanceofBread_Bread()
     {
-      Bread newBread = new Bread();
+      Bread newBread = new Bread(0);
       Assert.AreEqual(typeof(Bread), newBread.GetType());
     }
 
     [TestMethod]
-    public void BreadOrder_TotalForOneBread_LoafCost()
-    { 
-      Bread newBread = new Bread();
-      int loafOrder = 1;
-      Assert.AreEqual(5, Bread.BreadOrder(loafOrder));
-    }
-
-    [TestMethod]
-    public void BreadOrder_TotalForTwoBread_LoafCost()
-    { 
-      Bread newBread = new Bread();
-      int loafOrder = 2;
-      Assert.AreEqual(10, Bread.BreadOrder(loafOrder));
-    }
-
-    [TestMethod]
-    public void BreadOrder_TotalForThreeBread_LoafCost()
-    { 
-      Bread newBread = new Bread();
-      int loafOrder = 3;
-      Assert.AreEqual(10, Bread.BreadOrder(loafOrder));
-    }
-
-    [TestMethod]
-    public void BreadOrder_TotalForFourBread_LoafCost()
-    { 
-      Bread newBread = new Bread();
-      int loafOrder = 4;
-      Assert.AreEqual(15, Bread.BreadOrder(loafOrder));
-    }
-
-    [TestMethod]
-    public void BreadOrder_TotalForFiveBread_LoafCost()
+    public void CalculateTotal_TotalFor1Loaf_LoafCost()
     {
-      Bread newBread = new Bread();
-      int loafOrder = 5;
-      Assert.AreEqual(20, Bread.BreadOrder(loafOrder));
+      Bread newBread = new Bread(1);
+      Assert.AreEqual(5, newBread.CalculateTotal());
     }
 
     [TestMethod]
-    public void BreadOrder_TotalForSixBread_LoafCost()
+    public void CalculateTotal_TotalFor2Loaves_LoafCost()
     {
-      Bread newBread = new Bread();
-      int loafOrder = 6;
-      Assert.AreEqual(20, Bread.BreadOrder(loafOrder));
+      Bread newBread = new Bread(2);
+      Assert.AreEqual(10, newBread.CalculateTotal());
     }
 
     [TestMethod]
-    public void BreadOrder_TotalForSevenBread_LoafCost()
+    public void CalculateTotal_TotalFor3Loaves_LoafCost()
     {
-      Bread newBread = new Bread();
-      int loafOrder = 7;
-      Assert.AreEqual(25, Bread.BreadOrder(loafOrder));
+      Bread newBread = new Bread(3);
+      Assert.AreEqual(10, newBread.CalculateTotal());
     }
 
     [TestMethod]
-    public void BreadOrder_TotalForAnyBread_LoafCost()
+    public void CalculateTotal_TotalFor4_LoafCost()
     {
-      Bread newBread = new Bread();
-      int loafOrder = 0; //final test for >=3 ?
-      Assert.AreEqual(0, Bread.BreadOrder(loafOrder));
+      Bread newBread = new Bread(4);
+      Assert.AreEqual(15, newBread.CalculateTotal());
+    }
+
+    [TestMethod]
+    public void CalculateTotal_TotalFor5Loaves_LoafCost()
+    {
+      Bread newBread = new Bread(5);
+      Assert.AreEqual(20, newBread.CalculateTotal());
+    }
+    
+    [TestMethod]
+    public void CalculateTotal_TotalFor6Loaves_LoafCost()
+    {
+      Bread newBread = new Bread(6);
+      Assert.AreEqual(20, newBread.CalculateTotal());
+    }
+    [TestMethod]
+    public void CalculateTotal_TotalFor7Loaves_LoafCost()
+    {
+      Bread newBread = new Bread(7);
+      Assert.AreEqual(25, newBread.CalculateTotal());
+    }
+    [TestMethod]
+    public void CalculateTotal_TotalFor8Loaves_LoafCost()
+    {
+      Bread newBread = new Bread(8);
+      Assert.AreEqual(30, newBread.CalculateTotal());
+    }
+
+    [TestMethod]
+    public void CalculateTotal_TotalFor9Loaves_LoafCost()
+    {
+      Bread newBread = new Bread(9);
+      Assert.AreEqual(30, newBread.CalculateTotal());
+    }
+    
+    [TestMethod]
+    public void CalculateTotal_TotalFor10Loaves_LoafCost()
+    {
+      Bread newBread = new Bread(10);
+      Assert.AreEqual(35, newBread.CalculateTotal());
+    }
+    [TestMethod]
+    public void CalculateTotal_TotalFor15Loaves_LoafCost()
+    {
+      Bread newBread = new Bread(15);
+      Assert.AreEqual(50, newBread.CalculateTotal());
     }
   }
 }
