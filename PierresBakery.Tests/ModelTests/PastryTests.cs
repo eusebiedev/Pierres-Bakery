02fbc55a0cfeaef21a52e336e506be010790aa8e
Eusebie Siebenberg <siebenee@gmail.com>
2023-05-13 12:15:18 -0700

    update tests to account for large or any amount of orders

--- PierresBakery.Tests/ModelTests/PastryTests.cs
+++ PierresBakery.Tests/ModelTests/PastryTests.cs
@@ -6,83 +6,67 @@ namespace PierresBakery.Tests
   [TestClass]
   public class PastryTests
   {
-    [TestMethod]
-    public void PastryConstructor_CreatesInstanceofPastry_Pastry()
+  [TestMethod]
+    public void PastryConstructor_CreateInstanceOfPastry_Pastry()
     {
-      Pastry newPastry = new Pastry();
+      Pastry newPastry = new Pastry(0);
       Assert.AreEqual(typeof(Pastry), newPastry.GetType());
     }
 
     [TestMethod]
-    public void PastryOrder_TotalForOnePastry_PastryCost()
+    public void CalculateTotal_TotalFor1Pastry_PastryCost()
     {
-      Pastry newPastry = new Pastry();
-      int pastryOrder = 1;
-      Assert.AreEqual(2, Pastry.PastryOrder(pastryOrder));
+      Pastry newPastry = new Pastry(1);
+      Assert.AreEqual(2, newPastry.CalculateTotal());
+    }
+
+    [TestMethod]
+    public void CalculateTotal_TotalFor2Pastry_PastryCost()
+    {
+      Pastry newPastry = new Pastry(2);
+      Assert.AreEqual(4, newPastry.CalculateTotal());
     }
     
     [TestMethod]
-    public void PastryOrder_TotalForTwoPastry_PastryCost()
+    public void CalculateTotal_TotalFor3Pastry_PastryCost()
     {
-      Pastry newPastry = new Pastry();
-      int pastryOrder = 2;
-      Assert.AreEqual(4, Pastry.PastryOrder(pastryOrder));
+      Pastry newPastry = new Pastry(3);
+      Assert.AreEqual(6, newPastry.CalculateTotal());
     }
 
     [TestMethod]
-    public void PastryOrder_TotalForThreePastry_PastryCost()
+    public void CalculateTotal_TotalFor4Pastry_PastryCost()
     {
-      Pastry newPastry = new Pastry();
-      int pastryOrder = 3;
-      Assert.AreEqual(6, Pastry.PastryOrder(pastryOrder));
+      Pastry newPastry = new Pastry(4);
+      Assert.AreEqual(6, newPastry.CalculateTotal());
     }
 
     [TestMethod]
-    public void PastryOrder_TotalForFourPastryDiscount_PastryCost()
+    public void CalculateTotal_TotalFor5Pastry_PastryCost()
     {
-      Pastry newPastry = new Pastry();
-      int pastryOrder = 4;
-      Assert.AreEqual(6, Pastry.PastryOrder(pastryOrder));
+      Pastry newPastry = new Pastry(5);
+      Assert.AreEqual(8, newPastry.CalculateTotal());
+    }
+    
+    [TestMethod]
+    public void CalculateTotal_TotalFor6Pastry_PastryCost()
+    {
+      Pastry newPastry = new Pastry(6);
+      Assert.AreEqual(10, newPastry.CalculateTotal());
     }
 
     [TestMethod]
-    public void PastryOrder_TotalForFivePastryDiscount_PastryCost()
+    public void CalculateTotal_TotalFor7Pastry_PastryCost()
     {
-      Pastry newPastry = new Pastry();
-      int pastryOrder = 5;
-      Assert.AreEqual(8, Pastry.PastryOrder(pastryOrder));
+      Pastry newPastry = new Pastry(7);
+      Assert.AreEqual(12, newPastry.CalculateTotal());
     }
 
     [TestMethod]
-    public void PastryOrder_TotalForSixPastryDiscount_PastryCost()
+    public void CalculateTotal_TotalFor8Pastry_PastryCost()
     {
-      Pastry newPastry = new Pastry();
-      int pastryOrder = 6;
-      Assert.AreEqual(10, Pastry.PastryOrder(pastryOrder));
-    }
-
-    [TestMethod]
-    public void PastryOrder_TotalForSevenPastryDiscount_PastryCost()
-    {
-      Pastry newPastry = new Pastry();
-      int pastryOrder = 7;
-      Assert.AreEqual(12, Pastry.PastryOrder(pastryOrder));
-    }
-
-    [TestMethod]
-    public void PastryOrder_TotalForEightPastryDiscount_PastryCost()
-    {
-      Pastry newPastry = new Pastry();
-      int pastryOrder = 8;
-      Assert.AreEqual(12, Pastry.PastryOrder(pastryOrder));
-    }
-
-    [TestMethod]
-    public void PastryOrder_TotalForAnyPastry_PastryCost()
-    {
-      Pastry newPastry = new Pastry();
-      int pastryOrder = 0; //final test for >=4 ?
-      Assert.AreEqual(0, Pastry.PastryOrder(pastryOrder));
+      Pastry newPastry = new Pastry(8);
+      Assert.AreEqual(12, newPastry.CalculateTotal());
     }
   }
 }
